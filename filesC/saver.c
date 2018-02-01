1baa8c6534f868d1c3e729dfc36b5fa15b847a25
axlmk <axel.gauthier@viacesi.fr>
2018-02-01 21:42:20 +0100

    Change static and fix execl

--- filesC/saver.c
+++ filesC/saver.c
@@ -6,41 +6,44 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-typedef enum screenType screenType;
+typedef enum screenType screenType; // enum simplifiant les types d'ecrans choisis
 enum screenType {
 	STATIC = 1,
 	DYNAMIC = 2,
 	INTERACTIVE = 3
 };
 
-void history(screenType Type) {
+void write_history(screenType Type) {
 	time_t *temps;
 	time(temps);
 	struct tm *t;
-	t = localtime(temps);
+	t = localtime(temps); // créer une variable temps a utiliser pour ecrire ans le fichier
 	FILE *f = fopen("../stat/logs.txt", "r+");
 	if (f==NULL) {
 		fprintf(stderr, "Une erreur est survenue lors de l'ouverture du fichier <logs>.\n");
 	} else {
-		fseek(f, 0, SEEK_END);
-		fprintf(f, "%d/%d/%d %d:%d:%d;%d;%s\n", t->tm_mday, t->tm_mon, (t->tm_year+1900), t->tm_hour, t->tm_min, t->tm_sec, Type, "qsdf");
+		fseek(f, 0, SEEK_END); //rajoute la date a la derniere ligne
+		fprintf(f, "%d/%d/%d %d:%d:%d;%d;%s\n", t->tm_mday, t->tm_mon, (t->tm_year+1900), t->tm_hour, t->tm_min, t->tm_sec, Type, "qsdf"); //ecrit l'heure du lancement du screen
 	}
+	fclose(f);
 }
+
 void execProcess(screenType Type) {
 	pid_t child_pid;
-	child_pid = fork();
+	child_pid = fork(); //cree un nouveau processus
 	if (child_pid==0) {
-		history(Type);//ecrire fichier
+		write_history(Type);
 	} else {
-		char *envPath = malloc(sizeof(char)*(strlen(getenv("EXIASAVER_HOME"))));
-		strcpy(envPath, getenv("EXIASAVER_HOME"));
+		char *envPath = malloc(sizeof(char)*(strlen("../bin/")));
+		strcpy(envPath, ("../bin/"));
 		if (Type == STATIC) {
 			envPath = realloc(envPath, sizeof(char)*(strlen(envPath)+strlen("static")));
 			strcat(envPath, "static");
-//			char *envPathPbm = malloc(sizeof(char)*(strlen(getenv("$EXIASAVER1_PBM"))));
-//			strcpy(envPathPbm, "$EXIASAVER1_PBM"); 
-//			strcat(envPathPbm, "img1.txt"); // random file
-			execl(envPath,  "", NULL);
+			char *envPathPbm = malloc(sizeof(char)*(strlen("../pbm/")));
+			strcpy(envPathPbm, "../pbm/");
+			envPathPbm = realloc(envPathPbm, sizeof(char)*(strlen(envPathPbm)+strlen("img1.txt"))); // random file
+			strcat(envPathPbm, "img1.txt");
+			execl("static", "../pbm/img1.txt", NULL);
 		} else if (Type == DYNAMIC) {
 			strcat(envPath, "/dynamic");
 			execl(envPath,"", NULL);
@@ -52,44 +55,44 @@ void execProcess(screenType Type) {
 		} else {
 			fprintf(stderr, "Erreur, le type choisie n'existe pas.\n");
 		}
-		fprintf(stderr, "Une erreur est survenue lors de l'execution du nouveau processus.Pid : %d\n", child_pid);
+		// fprintf(stderr, "Une erreur est survenue lors de l'execution du nouveau processus. Pid : %d\n", child_pid);
 	}
 }
 
 int main (int argc, char *argv[]) {
 	system("clear");
-	if (argc == 2) {
+	if (argc == 2) { //verifie s'il y a des options lors l'ouverture du fichier
 		if(!strcmp(argv[1], "-stat")) {
 			FILE *logs = fopen("logs.txt", "r");
-			while (fgetc(logs)!=EOF) {
+			while (fgetc(logs)!=EOF) { //lis le fichier log.txt
 				fseek(logs, -1, SEEK_CUR);
 				printf("%c", fgetc(logs));
 			}
 			fclose(logs);
 			return EXIT_SUCCESS;
-		} else if(!strcmp(argv[1], "--help")) {
+		} else if(!strcmp(argv[1], "--help")) { //donne les options disponibles
 			printf("Les options disponibles sont :\n");
 			printf("-stat	Affiche les statistiques d'utilisation du saver.\n");
-		} else {
+		} else { //gerer l'erreur
 			fprintf(stderr, "L'option choisie n'existe pas, tapez <--help> pour voir les options disponibles.\n");
 			return EXIT_FAILURE;
 		}
-	} else if (argc == 1) {
+	} else if (argc == 1) { // lance les screensavers
 		srand(time(NULL));
 		int r=rand()%3+1;
 		int i=0;
-		r = 1;
+		r = 1; // r prends une variable aléatoire qui va servir a choisir le screen
 		if (r==1) {
 			execProcess(STATIC);
 		} else if (r==2) {
 			execProcess(DYNAMIC);
 		} else if (r==3) {
 			execProcess(INTERACTIVE);
-		} else {
+		} else { //gere l'erreur
 			fprintf(stderr, "Il s'est produit une erreur lors du chargement de <r>.\n");
 			return EXIT_FAILURE;
 		}
-	} else {
+	} else { // gere l'erreur
 		fprintf(stderr, "Le nombre d'arguments est incorrect. Il ne peut y avoir qu'un seul argument fournit.\n");
 		return EXIT_FAILURE;
 	}
--- filesC/static.c
+++ filesC/static.c
@@ -2,5 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	printf("Coucou, t'es dans le static\n");
+	system("clear");
+	FILE *f = fopen(argv[0], "r");
+	if(f==NULL) {
+		fprintf(stderr, "Une erreur est survenue lors de l'ouverture du fichier.\n");
+		return EXIT_FAILURE;
+	} else  {
+		fseek(f, 0, SEEK_SET);
+		while (fgetc(f)!=EOF) { //lis le fichier log.txt
+			fseek(f, -1, SEEK_CUR);
+			printf("%c", fgetc(f));
+		}
+	}
+	return EXIT_SUCCESS;
 }
