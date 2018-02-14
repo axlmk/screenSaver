7808f88fd3e409da6017e6b4f3d639fa0062a677
axlmk <axel.gauthier@viacesi.fr>
2018-02-14 22:34:57 +0100

    ajout fontions sur tableau -static

--- filesC/static.c
+++ filesC/static.c
@@ -4,24 +4,64 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-char *createArray(FILE f*, int N) {
+char *createArray(FILE *f, int N) {
 	char *array = malloc(sizeof(char)*(N+1));
 	fseek(f, 0, SEEK_SET);
-	while(fgetc(f)!=EOF) {
-		fseek(f, -1, SEEK_CUR)
+	int i=0;while(fgetc(f)!=EOF) {
+		fseek(f, -1, SEEK_CUR);
 		array[i]=fgetc(f);
+		i++;
 	}
 	return array;
 }
 
-char *eraseSharp(char *array) {
+char *takeOffData(char *array) {
 
-	return
 }
-}
-int magicNumber(FILE *f) {
 
-	return magic;
+void getDataPbm(char *array, char *magicNumber, int *width, int *lenght) {
+	int i=0, j=0, n=0;
+	char buffer[10] = "\0";
+	while(array[i]!='\n') {
+		buffer[j]=array[i];
+		i++;j++;
+	}i++; strcpy(magicNumber, buffer); memset(buffer, 0, 10); j=0;//magicNumber
+
+	while(array[i]!='\n') {
+		buffer[j]=array[i];
+		i++;j++;
+	}i++; *width = atoi(buffer); memset(buffer, 0, 10), j=0;
+
+	while(array[i]!='\n') {
+		buffer[j]=array[i];
+		i++;j++;
+	}*lenght = atoi(buffer);
+
+}
+
+int lenFile(FILE *f) {
+	fseek(f, 0, SEEK_END);
+	int n = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	return n;
+
+}
+
+char *eraseSharp(char *array, int N) { //si # a la derniere ligne on a un \n en trop car il va garder le dernier \n "enregistré"
+	int i=0, j=0;
+	char *buffer = malloc(sizeof(char)*N);
+	memset(buffer, 0, N);
+	while(array[i]!='\0') {
+		if(array[i]=='#') {
+			while(array[i]!='\n' && array[i]!='\0') {
+				i++;
+			} i++;
+		} else {
+			buffer[j] = array[i];
+			j++;i++;
+		}
+	}
+	return buffer;
 }
 
 int main(int argc, char *argv[]) {
@@ -33,9 +73,10 @@ int main(int argc, char *argv[]) {
 	} else  {
 		struct winsize w;
     	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-		char magicNumber[3] = "\0";
-		int width, height;
-		}
+		char magicNumber[10] = "\0";
+		int width, length;
+		char *array = eraseSharp(createArray(f, lenFile(f)), lenFile(f));
+		getDataPbm(array, magicNumber, &width, &length);
 	}
 	return EXIT_SUCCESS;
 }
